9f6d3890f5c20a409484a359ed6b9b45fa45e5da
Eshna <arfanoorchy@gmail.com>
2020-11-21 23:47:10 +0600

    Arfatur Noor committed

---
+++ Admin/course_enrollment.php
@@ -0,0 +1,250 @@
+<!DOCTYPE HTML>
+<html lang="en">
+		<head>
+			<title>
+				Report
+			</title>
+			<style>
+			body{
+				margin:10px;
+			}
+			.cell{
+				position: relative;
+				left:0;
+				right:0;
+				margin: auto;
+				width:1080px;
+			}
+			table,th,td {
+				border: 2px solid black;
+				border-collapse: collapse;
+			}
+			.content-table{
+				float:left;
+				margin:10px;
+			}
+			
+			</style>
+			
+		</head>
+	<body>
+	
+	<h1 align="center">Monthly / Yearly Course Enrollment</h1>
+	<div class="cell">
+		<table class="content-table">
+			<tr>
+				<th>Subject</th>
+				<th>&nbsp;Section&nbsp;</th>
+				<th>&nbsp;Enroll&nbsp;</th>
+			</tr>
+			<tr>
+				<td rowspan="3">DATA STRUCTURE</td>
+				<td>A</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>29</td>
+			</tr>
+			<tr>
+				<td rowspan="4">BANGLADESH STUDIES</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>39</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+			<tr>
+				<td rowspan="4">DISCRETE MATHEMATICS</td>
+				<td>A</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>37</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>31</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>35</td>
+			</tr>
+			<tr>
+				<td rowspan="4">ELECTRONIC DEVICES</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+		</table>
+		
+		<table class="content-table">
+			<tr>
+				<th>Subject</th>
+				<th>&nbsp;Section&nbsp;</th>
+				<th>&nbsp;Enroll&nbsp;</th>
+			</tr>
+			<tr>
+				<td rowspan="3">BUSINESS COMMUNICATION</td>
+				<td>A</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>29</td>
+			</tr>
+			<tr>
+				<td rowspan="4">ALGORITHMS</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>39</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+			<tr>
+				<td rowspan="4">ELECTRICAL CIRCUITS</td>
+				<td>A</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>37</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>31</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>35</td>
+			</tr>
+			<tr>
+				<td rowspan="4">PRINCIPLES OF ECONOMICS</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+		</table>
+		<table class="content-table">
+			<tr>
+				<th>Subject</th>
+				<th>&nbsp;Section&nbsp;</th>
+				<th>&nbsp;Enroll&nbsp;</th>
+			</tr>
+			<tr>
+				<td rowspan="3">WEB TECHNOLOGIES</td>
+				<td>A</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>29</td>
+			</tr>
+			<tr>
+				<td rowspan="4">OPERATING SYSTEM</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>39</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+			<tr>
+				<td rowspan="4">PHYSICS</td>
+				<td>A</td>
+				<td>33</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>37</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>31</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>35</td>
+			</tr>
+			<tr>
+				<td rowspan="4">THEORY OF COMPUTATION</td>
+				<td>A</td>
+				<td>38</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>34</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>36</td>
+			</tr>
+		</table>
+		</div>
+	</body>
+</html>
